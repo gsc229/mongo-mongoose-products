64066821e10217fbcb21f711d828be56145c4958
Greg <gsc229@gmail.com>
2019-12-23 02:13:01 -0500

    changed Product schema to take an array of images

--- controllers/products.js
+++ controllers/products.js
@@ -8,8 +8,11 @@ const asyncHandler = require('../middleware/async'); // keeps code DRY
 // @route   GET /api/v1.0/vendors/:vendorId/products
 // @access  Public
 exports.getAllProducts = asyncHandler(async (req, res, next) => {
+
+    console.log('getAllPs req.query: ', req.query);
+
     let query;
-    if(req.params.vendorId) {
+    if (req.params.vendorId) {
         query = Product.find({
             vendor: req.params.vendorId
         })
@@ -33,20 +36,49 @@ exports.getAllProducts = asyncHandler(async (req, res, next) => {
 // @route   GET /api/v1.0/products/:id
 // @access  Public
 exports.getProduct = asyncHandler(async (req, res, next) => {
-   const product = await Product.findById(req.params.id).populate({
-       path: 'vendor',
-       select: 'business_name description'
-   });
+    const product = await Product.findById(req.params.id).populate({
+        path: 'vendor',
+        select: 'business_name description'
+    });
 
-   if(!product) {
-       return next(new ErrorResponse(`No product with the id of ${req.params.id}`),
-       404
-       );
-   }
+    if (!product) {
+        return next(new ErrorResponse(`No product with the id of ${req.params.id}`),
+            404
+        );
+    }
 
     res.status(200).json({
         success: true,
         count: product.length,
         data: product
     });
+});
+
+
+// @desc    Add a new product
+// @route   POST /api/v1.0/products
+// @access  Public
+exports.addProduct = asyncHandler(async (req, res, next) => {
+    const product = await Product.create(req.body);
+    console.log(product.product_images.typof);
+    res.status(200).json({
+        success: true,
+        count: product.length,
+        data: product
+    })
+})
+
+
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if (!product) {
+        return next(
+            new ErrorResponse(`Product not found with the id of ${req.params.id}`, 404)
+        );
+    }
+    res.status(200).json({ success: true, data: product });
 });
